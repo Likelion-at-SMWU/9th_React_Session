6f48ebf889c3398bf70a329194d09944f9123eb4
윤정인 <yoonjeongin@naver.com>
2021-11-09 14:18:36 +0900

    7_API Integrate

---
+++ session07/src/Users.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import { useAsync } from 'react-async';
+import User from './User';
+
+
+async function getUsers() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+  return response.data;
+}
+
+function Users() {
+  const [userId, setUserId] = useState(null);
+  const { data: users, error, isLoading, run } = useAsync({
+    deferFn: getUsers
+  });
+
+  if (isLoading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return <button onClick={run}>불러오기</button>;
+  return (
+    <>
+      <ul>
+        {users.map(user => (
+          <li
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+            style={{ cursor: 'pointer' }}
+          >
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={run}>다시 불러오기</button>
+      {userId && <User id={userId} />}
+    </>
+  );
+}
+
+export default Users;
+
+//1. API 연동 기본
+//2. useReducer로 요청상태 관리하기
+//3. useAsync 커스텀 Hook 만들기
+//4. react-async로 요청상태 관리하기
+//5. Context와 함께 사용하기
